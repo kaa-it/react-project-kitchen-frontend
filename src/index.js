3bea8708f34546164c17d0d37ec69719a5c2bd68
Andrey Kruglov <kruglov.it@gmail.com>
2021-09-18 03:08:00 +0300

    update libraries #16

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { store, history} from './store';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import React from "react";
+import { store, history } from "./store";
+import { Route, Switch } from "react-router-dom";
+import "./fonts/fonts.css";
+import "./index.css";
+import App from "./components/App";
+import { ConnectedRouter } from "connected-react-router";
 
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
-
-import './fonts/fonts.css';
-import './index.css';
-
-import App from './components/App';
-
-ReactDOM.render((
+ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={App} />
       </Switch>
     </ConnectedRouter>
-  </Provider>
+  </Provider>,
 
-), document.getElementById('root'));
+  document.getElementById("root")
+);
