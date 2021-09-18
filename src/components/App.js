48730c7033a86157c5f4c85f03d38959ff9364bc
edword03 <veretennikov_dmitriy01@mail.ru>
2021-09-18 19:04:54 +0400

    refactoring app component #1

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
-import agent from "../agent";
-import Header from "./Header/Header";
-import React from "react";
-import { connect } from "react-redux";
-import { APP_LOAD, REDIRECT } from "../constants/actionTypes";
-import { Route, Switch } from "react-router-dom";
-import Article from "../components/Article";
-import Editor from "../components/Editor";
-import Home from "../components/Home";
-import Login from "../components/Login";
-import Profile from "../components/Profile";
-import ProfileFavorites from "../components/ProfileFavorites";
-import Register from "../components/Register";
-import Settings from "../components/Settings";
-import { store } from "../store";
-import { push } from "connected-react-router";
+import agent from '../agent';
+import Header from './Header/Header';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { APP_LOAD, REDIRECT } from '../constants/actionTypes';
+import { Route, Switch } from 'react-router-dom';
+import Article from '../components/Article';
+import Editor from '../components/Editor';
+import Home from '../components/Home';
+import Login from '../components/Login';
+import Profile from '../components/Profile';
+import ProfileFavorites from '../components/ProfileFavorites';
+import Register from '../components/Register';
+import Settings from '../components/Settings';
+import { store } from '../store';
+import { push } from 'connected-react-router';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     appLoaded: state.common.appLoaded,
     appName: state.common.appName,
@@ -24,38 +25,35 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  onLoad: (payload, token) =>
-    dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) => dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
   onRedirect: () => dispatch({ type: REDIRECT }),
 });
 
-class App extends React.Component {
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.redirectTo) {
-      // this.context.router.replace(nextProps.redirectTo);
-      store.dispatch(push(nextProps.redirectTo));
-      this.props.onRedirect();
-    }
-  }
-
-  componentWillMount() {
-    const token = window.localStorage.getItem("jwt");
+const App = ({ appLoaded, appName, currentUser, onLoad, redirectTo, onRedirect }) => {
+  useEffect(() => {
+    const token = window.localStorage.getItem('jwt');
     if (token) {
       agent.setToken(token);
     }
 
-    this.props.onLoad(token ? agent.Auth.current() : null, token);
-  }
+    onLoad(token ? agent.Auth.current() : null, token);
+    console.log(redirectTo);
+  }, [])
 
-  render() {
-    if (this.props.appLoaded) {
-      return (
+  useEffect(() => {
+    if (redirectTo) {
+      // this.context.router.replace(nextProps.redirectTo);
+      store.dispatch(push(redirectTo));
+      onRedirect();
+    }
+  }, [redirectTo])
+
+  return (
+    <>
+      {appLoaded ? (
         <React.Fragment>
-          <Header
-            appName={this.props.appName}
-            currentUser={this.props.currentUser}
-          />
+          <Header appName={appName} currentUser={currentUser} />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
@@ -68,19 +66,25 @@ class App extends React.Component {
             <Route path="/@:username" component={Profile} />
           </Switch>
         </React.Fragment>
-      );
-    }
-    return (
-      <Header
-        appName={this.props.appName}
-        currentUser={this.props.currentUser}
-      />
-    );
-  }
-}
+      ) : (
+        <Header appName={appName} currentUser={currentUser} />
+      )}
+    </>
+  );
+};
 
-// App.contextTypes = {
-//   router: PropTypes.object.isRequired
-// };
+
+App.propTypes = {
+  appName: PropTypes.string,
+  onLoad: PropTypes.func,
+  appLoaded: PropTypes.bool,
+  currentUser: PropTypes.shape({
+    email: PropTypes.string,
+    image: PropTypes.string,
+    token: PropTypes.string,
+    username: PropTypes.string
+  }),
+  onRedirect: PropTypes.func
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/types.js
+++ src/types.js
@@ -3,5 +3,5 @@ import PropTypes from "prop-types";
 export const userPropTypes = PropTypes.shape({
   email: PropTypes.string.isRequired,
   token: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
+	username: PropTypes.string.isRequired,
 });
