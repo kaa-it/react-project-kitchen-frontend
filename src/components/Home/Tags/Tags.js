18d0ae8572f136c0fce1dcad52a68e227a0e8276
Andrey Kruglov <kruglov.it@gmail.com>
2021-10-20 02:51:18 +0300

    fix TagList click tab callback and store fix

--- src/components/Home/Tags/Tags.js
+++ src/components/Home/Tags/Tags.js
@@ -1,41 +1,43 @@
-import React from 'react';
+import React from "react";
 import styles from "./Tags.module.css";
-import PropTypes from 'prop-types';
-import TagList from '../../common/TagList/TagList'
+import PropTypes from "prop-types";
+import TagList from "../../common/TagList/TagList";
+import agent from "../../../agent";
 
-const Tags = props => {
-
-  const tags = props.tags;
-  // const tags = ['Tag', 'Test', 'YOLO', 'WTF', 'GM', 'TGIF', 'ASAP', 'DIY']
+const Tags = ({ tags, onClickTag }) => {
+  const handleClickTag = (tag) => {
+    onClickTag(
+      tag,
+      (page) => agent.Articles.byTag(tag, page),
+      agent.Articles.byTag(tag)
+    );
+  };
 
   return (
     <div>
-      {tags ?
+      {tags ? (
         <div className={styles.container}>
-          {tags.length < 1 ?
+          {tags.length < 1 ? (
             <p className={styles.title}>Список тегов пуст</p>
-          :
+          ) : (
             <>
               <p className={styles.title}>Популярные теги</p>
-              <TagList                   
-                tags={ tags}
-                onClickTag={props.onClickTag}
-              />
+              <TagList tags={tags} onClickTag={handleClickTag} />
             </>
-          }
+          )}
         </div>
-        :
+      ) : (
         <div className={styles.container}>
           <p className={styles.title}>Загрузка тегов...</p>
         </div>
-      }
+      )}
     </div>
   );
 };
 
 Tags.propTypes = {
   tags: PropTypes.array,
-  onClickTag: PropTypes.func.isRequired
+  onClickTag: PropTypes.func.isRequired,
 };
 
 export default Tags;
--- src/components/common/TagList/TagList.js
+++ src/components/common/TagList/TagList.js
@@ -1,39 +1,31 @@
-import React from 'react';
-import agent from '../../../agent';
+import React from "react";
 import styles from "./TagList.module.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const TagList = props => {
-
-  const tags = props.tags;
-
+const TagList = ({ tags, onClickTag }) => {
+  let props = {};
   return (
     <div className={styles.tags}>
-      {
-        tags.map(tag => {
-          const handleClick = ev => {
+      {tags.map((tag) => {
+        if (onClickTag) {
+          props.onClick = (ev) => {
             ev.preventDefault();
-            props.onClickTag(tag, page => agent.Articles.byTag(tag, page), agent.Articles.byTag(tag));
-          }
-          return (
-            <a
-              href=""
-              className={styles.tag}
-              key={tag}
-              onClick={props.onClickTag && handleClick}
-            >
-              {tag}
-            </a>
-          );
-        })
-      }
+            onClickTag(tag);
+          };
+        }
+        return (
+          <a href="" className={styles.tag} key={tag} {...props}>
+            {tag}
+          </a>
+        );
+      })}
     </div>
   );
 };
 
 TagList.propTypes = {
   tags: PropTypes.array,
-  onClickTag: PropTypes.func
+  onClickTag: PropTypes.func,
 };
 
 export default TagList;
