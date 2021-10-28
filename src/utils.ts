f6dac22b815a5f4d41184347dc37314ef0b1128a
Andrey Kruglov <kruglov.it@gmail.com>
2021-10-28 12:11:16 +0300

    convert getColorByType to TS

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,8 @@
-const getColorByType = (type: string) => {
+const getColorByType = (type: "primary" | "secondary") => {
   if (type === "primary") {
     return "#F2F2F3";
-  } else if (type === "secondary") {
-    return "#8585AD";
   } else {
-    return "#F2F2F3";
+    return "#8585AD";
   }
 };
 
