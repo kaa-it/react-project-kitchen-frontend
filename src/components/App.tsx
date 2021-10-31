8065788835ba02643c4b8713fcd038a69d8185d5
Andrey Kruglov <kruglov.it@gmail.com>
2021-10-31 12:39:55 +0300

    fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import { Route, Switch, useHistory } from "react-router-dom";
 import Article from "../components/Article";
 import Editor from "../components/Editor";
 import Home from "./Home";
-import Login from "../components/Login";
+import Login from "../components/Login/Login";
 import Profile from "./Profile/Profile";
 import ProfileFavorites from "./Profile/ProfileFavorites";
 import Register from "../components/Register/Register";
