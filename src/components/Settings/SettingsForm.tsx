caf81a4ee6269427a03834b9b388be15f2eb2350
Andrey Kruglov <kruglov.it@gmail.com>
2021-11-01 19:36:42 +0300

    fix

--- src/components/Settings/SettingsForm.tsx
+++ src/components/Settings/SettingsForm.tsx
@@ -1,98 +1,103 @@
-import React, { FC, ChangeEvent, useEffect, useState, FormEvent } from 'react';
-import { TCurrentUser } from '.';
-import { useAppSelector } from '../../services';
-import { saveSettings } from '../../services/commonSlice';
-import agent from '../../agent';
-import { useDispatch } from 'react-redux';
+import React, { FC, ChangeEvent, useEffect, useState, FormEvent } from "react";
+import { TCurrentUser } from ".";
+import { useAppSelector } from "../../services";
+import { saveSettings } from "../../services/commonSlice";
+import agent from "../../agent";
+import { useDispatch } from "react-redux";
 
 const SettingsForm: FC = () => {
+  const { currentUser, inProgress } = useAppSelector((state) => state.common);
+  const dispatch = useDispatch();
 
-    const { currentUser, inProgress } = useAppSelector(state => state.common);
-    const dispatch = useDispatch();
+  const [state, setState] = useState<TCurrentUser>({
+    image: "",
+    username: "",
+    bio: "",
+    email: "",
+    password: "",
+  });
 
-    const [state, setState] = useState<TCurrentUser>({
-      image: '',
-      username: '',
-      bio: '',
-      email: '',
-      password: ''
-    });
-  
-    const updateState = (field: string) => (ev: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setState({ ...state, [field]: ev.target.value});
-  
-    useEffect(() => {
-      if (currentUser) {
-        setState({...state, ...currentUser})
-      }
-    }, [currentUser]);
-  
-    const submitForm = (ev: FormEvent) => {
-      ev.preventDefault();
-  
-      const fetcher = agent.Auth.save(state);
-      dispatch(saveSettings({ fetcher }))
-    };
-  
-    return (
-      <form onSubmit={submitForm}>
-        <fieldset>
-          <fieldset className="form-group">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="URL иззображения профиля"
-              value={state.image}
-              onChange={updateState('image')} />
-          </fieldset>
-  
-          <fieldset className="form-group">
-            <input
-              className="form-control form-control-lg"
-              type="text"
-              placeholder="Username"
-              value={state.username}
-              onChange={updateState('username')} />
-          </fieldset>
-  
-          <fieldset className="form-group">
-            <textarea
-              className="form-control form-control-lg"
-              rows={8}
-              placeholder="Информация о вас"
-              value={state.bio}
-              onChange={updateState('bio')}>
-            </textarea>
-          </fieldset>
-  
-          <fieldset className="form-group">
-            <input
-              className="form-control form-control-lg"
-              type="email"
-              placeholder="Email"
-              value={state.email}
-              onChange={updateState('email')} />
-          </fieldset>
-  
-          <fieldset className="form-group">
-            <input
-              className="form-control form-control-lg"
-              type="password"
-              placeholder="Новый пароль"
-              value={state.password}
-              onChange={updateState('password')} />
-          </fieldset>
-  
-          <button
-            className="btn btn-lg btn-primary pull-xs-right"
-            type="submit"
-            disabled={inProgress || false}
-          > 
-            Сохранить
-          </button>
-  
+  const updateState =
+    (field: string) =>
+    (ev: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      setState({ ...state, [field]: ev.target.value });
+
+  useEffect(() => {
+    if (currentUser) {
+      setState({ ...state, ...currentUser });
+    }
+  }, [currentUser]);
+
+  const submitForm = (ev: FormEvent) => {
+    ev.preventDefault();
+
+    const fetcher = agent.Auth.save(state);
+    dispatch(saveSettings({ fetcher }));
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <fieldset>
+        <fieldset className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="URL иззображения профиля"
+            value={state.image}
+            onChange={updateState("image")}
+          />
         </fieldset>
-      </form>
-    );
-  }
 
-  export default SettingsForm;
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="text"
+            placeholder="Username"
+            value={state.username}
+            onChange={updateState("username")}
+          />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <textarea
+            className="form-control form-control-lg"
+            rows={8}
+            placeholder="Информация о вас"
+            value={state.bio}
+            onChange={updateState("bio")}
+          />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="email"
+            placeholder="Email"
+            value={state.email}
+            onChange={updateState("email")}
+          />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="password"
+            placeholder="Новый пароль"
+            value={state.password}
+            onChange={updateState("password")}
+          />
+        </fieldset>
+
+        <button
+          className="btn btn-lg btn-primary pull-xs-right"
+          type="submit"
+          disabled={inProgress || false}
+        >
+          Сохранить
+        </button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default SettingsForm;
--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,15 +1,15 @@
-import React, { FC, useEffect } from 'react';
-import ListErrors from '../ListErrors';
-import { TUser } from '../../types';
-import SettingsForm from './SettingsForm';
-import { useAppDispatch, useAppSelector } from '../../services';
-import { unloadSettingsPage } from '../../services/commonSlice';
-import { logout } from '../../services/commonSlice';
+import React, { FC, useEffect } from "react";
+import ListErrors from "../ListErrors";
+import { TUser } from "../../types";
+import SettingsForm from "./SettingsForm";
+import { useAppDispatch, useAppSelector } from "../../services";
+import { unloadSettingsPage } from "../../services/commonSlice";
+import { logout } from "../../services/commonSlice";
 
-export type TCurrentUser = Omit<TUser, 'token'> & { password: string }
+export type TCurrentUser = Omit<TUser, "token"> & { password: string };
 
 const Settings: FC = () => {
-  const { currentUser, inProgress, errors } = useAppSelector(state => state.common);
+  const { errors } = useAppSelector((state) => state.common);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -27,25 +27,21 @@ const Settings: FC = () => {
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-
             <h1 className="text-xs-center">Ваши настройки</h1>
 
-            <ListErrors errors={errors}></ListErrors>
+            <ListErrors errors={errors} />
 
             <SettingsForm />
             <hr />
 
-            <button
-              className="btn btn-outline-danger"
-              onClick={onClickLogout}>
+            <button className="btn btn-outline-danger" onClick={onClickLogout}>
               Выйти из аккаунта
             </button>
-
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Settings;
